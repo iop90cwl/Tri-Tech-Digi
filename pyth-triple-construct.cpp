247c7e2a4e223876540ec6081bd872e0ab8bb492
iop90cwl <iop90cwl@charter.net>
2014-11-21 12:31:32 -0800

    Pythagorean Triple Construct

---
+++ pyth-triple-construct.cpp
@@ -0,0 +1,40 @@
+// example: Pythagorean Triple Check  constructor
+//  This gives a better hint on how to display the text output
+// save file as pyth-triple-construct-intials.cpp  example pyth-triple-construct-cwc.cpp )
+// https://github.com/tritechsc/PythagoreanTriple
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Pythagorean {
+    int x, y;
+  public:
+    Pythagorean (int,int);
+    int ic () {return sqrt(x*x + y*y);}
+    double c () {return sqrt(x*x+y*y);}
+};
+Pythagorean::Pythagorean (int a, int b) {
+  x = a;
+  y = b;
+}
+
+int main () {
+
+int a,b;
+//*********************
+for (b = 1; b <= 1000000; b++){
+
+ // add a for loop incrementing b
+  for (a = 1; a <= 1000000; a++){
+    Pythagorean triangle (a,b);
+    //*********************
+    if (triangle.c() == triangle.ic()){
+        cout << "a b c " << a<<" " <<b <<" "<<triangle.c() <<  "  "<<triangle.ic() << endl;
+    }
+  }
+}
+  return 0;
+}
+//wire code to display the Pythagorian Tiples with values of a and b ranging from 1 to 200
+// Save the output as a text file names initial-pythagorenatiples.txt
+//  Generate the text output as follows: C:\pathtocode\pyth-triple-construct-cwc.exe > pythagorenatiples.txt
